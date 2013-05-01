de1dddad317c51dcbd599bf65a8cd5bb61b03d00
Jeanneth <lajane2020@hotmail.com>
2013-05-01 16:40:37 -0500

    listado de medidores

--- src/Payment/MeterBundle/Controller/MeterController.php
+++ src/Payment/MeterBundle/Controller/MeterController.php
@@ -2,6 +2,8 @@
 
 namespace Payment\MeterBundle\Controller;
 
+use Payment\MeterBundle\Form\Type\MeterSearchType;
+use Payment\MeterBundle\Entity\MeterSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Payment\ApplicationBundle\Libraries\Paginator\Paginator;
@@ -13,8 +15,39 @@ use Payment\DataAccessBundle\Entity\Member;
 
 class MeterController extends Controller
 {
-    public function indexAction($name)
+	/**
+	 * @Template()
+	 * @Secure(roles="ROLE_ADMIN")
+	 */
+    public function listMeterAction(Request $request)
     {
-        return $this->render('PaymentMeterBundle:Default:index.html.twig', array('name' => $name));
+    	$meterEntity = new MeterSearch();
+    	$limit = self::LIMIT_PAGINATOR;
+        $offset = 0;
+        $meterNumber = null;
+        
+        $meterForm = $this->createForm(new MeterSearchType(), $meterEntity);
+        if ($request->getMethod() == 'POST') 
+        {
+            $meterForm->bind($request);
+            $datas = $meterForm->getData();
+            if ($meterForm->isValid())
+            {
+                $meterNumber = $datas->getNumber();
+                $offset = $datas->getOffset();
+                $limit = $datas->getLimit();
+            }
+        }
+        $offsetItem = $offset;
+        if ($offsetItem > 0) 
+        {
+            $offsetItem = $offsetItem - 1;
+        }
+        $offsetItem = $offsetItem * $limit;
+        $total = $this->getDoctrine()->getManager()->getRepository('PaymentDataAccessBundle:Account')->findMeterByParametersToList($meterNumber, $offsetItem, $limit);
+        $total = $total[0][1];
+        $meter = $this->getDoctrine()->getManager()->getRepository('PaymentDataAccessBundle:Account')->findMeterByParametersToList($meterNumber, $offsetItem, $limit, false);
+        $paginator = new Paginator($meterForm->getName(), $total, $offset, $limit);
+        return array('form' => $meterForm->createView(), 'limit' => $limit, 'total' => $total, 'meter' => $meter, 'paginator' => $paginator);
     }
 }
