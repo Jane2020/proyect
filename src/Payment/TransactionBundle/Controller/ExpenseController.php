4816e998484521f2b616068c9b4c5d56b65815da
efaby <efaby10@gmail.com>
2013-05-12 22:25:26 -0500

    prueba desde linux 64

--- src/Payment/TransactionBundle/Controller/ExpenseController.php
+++ src/Payment/TransactionBundle/Controller/ExpenseController.php
@@ -2,11 +2,22 @@
 namespace Payment\TransactionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Payment\ApplicationBundle\Libraries\Paginator\Paginator;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class ExpenseController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('PaymentTransactionBundle:Default:index.html.twig', array('name' => $name));
-    }
+   const LIMIT_PAGINATOR = 20;
+	/**
+	 * @Template()
+	 * @Secure(roles="ROLE_ADMIN")
+	 */
+	public function listConsumptionAction(Request $request)
+	{
+		$consumptionEntity = new ConsumptionSearch();
+		
+	}
 }
