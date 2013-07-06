73d02e3f7f7e0d64f635416e929e95745efaf060
Jeanneth <lajane2020@hotmail.com>
2013-07-05 22:45:15 -0500

    Cambios en las alertas

--- src/Payment/PaymentBundle/Form/Type/PaymentTypeSearchType.php
+++ src/Payment/PaymentBundle/Form/Type/PaymentTypeSearchType.php
@@ -8,7 +8,7 @@ class PaymentTypeSearchType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-		$builder->add('name', 'text', array('label'=>utf8_encode('Nombre del Miembro:'), 'required'=>false, 'max_length'=>64));
+		$builder->add('name', 'text', array('label'=>utf8_encode('Nombre del Tipo de Pago:'), 'required'=>false, 'max_length'=>64));
 		$builder->add('offset','hidden');
 		$builder->add('limit','hidden');
 	}
